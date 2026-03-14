9988e6677de83311ece1440ab87110fef3688be7
Denilson Kuratomi <mhtaue@gmail.com>
2026-03-14 15:54:50 +0800

    git merge conflict + ssh enter passphrase ☠️

--- frontend/src/routes/console/workspace.tsx
+++ frontend/src/routes/console/workspace.tsx
@@ -8,8 +8,10 @@ export const Route = createFileRoute('/console/workspace')({
 function RouteComponent() {
   return (
     <>
-        <div className='w-full h-full relative'>
+        <div className='w-full h-full relative bg-black'>
             <StarField />
+
+            //ainda tenho q pensar no design... + configurar o ambiente pq o backend ta sem o docker p drizzle rodar. dps disso -- fzr o design, pensar no workflow das functions e fzr a API com fastify p functions. AMEM DEUS EU ODEIO git merge conflict
         </div>
     </>
   )
