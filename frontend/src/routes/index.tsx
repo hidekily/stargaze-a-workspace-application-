6ea92ade7816d593fce50f960c555982c570796e
Denilson Kuratomi <mhtaue@gmail.com>
2026-03-18 12:12:18 +0800

    responsive login page

--- frontend/src/routes/index.tsx
+++ frontend/src/routes/index.tsx
@@ -38,8 +38,8 @@ function RouteComponent() {
       <div className='relative bg-[#12121C] w-full h-full flex justify-center items-center'>
         <StarField />
 
-        <section className='relative z-10 w-full h-full flex flex-row'>
-          <section className='w-[50%] h-full flex flex-col p-14 gap-5'>
+        <section className='relative z-10 w-full h-full flex flex-col md:flex-row'>
+          <section className='w-[100%] md:w-[50%] h-[50%] md:h-[50%] flex flex-col p-14 gap-5'>
             <span className='text-[#FF6B4A] text-4xl'>Stargaze | ✨</span>
             {<span className='text-lg break-normal w-[90%] text-[#E8E8F0]'>
               Your space to
@@ -52,7 +52,7 @@ function RouteComponent() {
             </span>}
           </section>
 
-          <section className='w-[50%] h-full flex flex-col justify-center items-center opacity-80'>
+          <section className='w-full md:w-[50%] h-[50%] md:h-full flex flex-col justify-center items-center opacity-80'>
               <section className='rounded-2xl w-[80%] h-[65%] bg-[#1A1A2E] shadow-lg shadow-white flex flex-col justify-center items-center gap-5'>
                 <button onClick={() => tryLogin('google')} className='bg-white w-[80%] h-[15%] rounded-lg flex justify-center items-center text-center gap-2'>
                   <span className='google'></span>
