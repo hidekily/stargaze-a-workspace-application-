16e7d5f426618812a83ef1b50ae6bf16df97bef9
Denilson Kuratomi <mhtaue@gmail.com>
2026-03-20 19:15:06 +0800

    1/3 backend workspace app.post completed

--- backend/src/routes/workspace.ts
+++ backend/src/routes/workspace.ts
@@ -4,6 +4,7 @@ import { FastifyReply } from 'fastify';
 import { auth } from "shared/auth";
 import {db} from "shared/db"
 import { request } from 'http';
+import { workspace } from "shared/db/schema";
 
 
 export async function appFastify(app :FastifyInstance, request: FastifyReply){
@@ -15,8 +16,6 @@ export async function appFastify(app :FastifyInstance, request: FastifyReply){
         dayLimit: z.number().optional(),
         userInvLimit: z.number().optional()
     })
-
-
 
     app.post('/', async(request, reply) =>{
         const session = await auth.api.getSession({
@@ -33,7 +32,20 @@ export async function appFastify(app :FastifyInstance, request: FastifyReply){
         if(!response.success){
             return reply.status(400).send({error: "data not found"})
         }
+
+        const {workspaceName, type, memberLimit, img, dayLimit, userInvLimit} = response.data
+
+        const [newWorkspace] = await db.insert(workspace).values({
+            name: workspaceName,
+            type: type,
+            memberLimit: memberLimit,
+            id: workspaceId
+        }).returning()
+
+        return reply.status(201).send(newWorkspace)
     })
+
+
 
     app.get('/', async() =>{
 
