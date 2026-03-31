a690942793c6a277770cf2c74a908ea40160c44a
Denilson Kuratomi <mhtaue@gmail.com>
2026-03-31 16:42:19 +0800

    made active tab for better user experience

--- frontend/src/routes/console/workspace.tsx
+++ frontend/src/routes/console/workspace.tsx
@@ -1,5 +1,4 @@
 import { createFileRoute, useNavigate } from '@tanstack/react-router'
-import { StarField } from '@/components/Starfield'
 import { Outlet, Link } from '@tanstack/react-router'
 import { Session } from '@/types/session'
 import { useEffect, useState } from 'react'
@@ -12,6 +11,7 @@ export const Route = createFileRoute('/console/workspace')({
 function RouteComponent() {
   const [session, setSession] = useState<Session | null>()
   const navigate = useNavigate()
+  const [active, setIsActive] = useState<"P" | "S" | "W" | null>(null)
 
   async function fetchSesssion(){
     const {data} = await authClient.getSession();
@@ -31,23 +31,26 @@ function RouteComponent() {
                 <section className='w-[80%] md:w-full h-full md:h-[80%] flex flex-row md:flex-col items-center gap-8'>
                   {/*  */}
                     <Link to='/console/workspace/personal' 
-                      className='border-1 rounded-full h-full w-[20%] flex flex-col justify-center items-center lg:mt-5 md:h-[15%] md:w-[80%] text-[#FF6B4A]'>
+                      onClick={() => setIsActive('P')}
+                      className={`border-1 ${active === 'P' ? "bg-[#FF6B4A]/50" : "bg-[#FF6B4A]/10"} rounded-full h-full w-[20%] flex flex-col justify-center items-center lg:mt-5 md:h-[15%] md:w-[80%] text-[#FF6B4A]`}>
                       <span>P</span>
                     </Link>
 
                     <Link to='/console/workspace/job' 
-                      className='border-1 rounded-full h-full w-[20%] flex flex-col justify-center items-center md:h-[15%] md:w-[80%] text-[#4A6BFF]'>
+                      onClick={() => setIsActive('W')}
+                      className={`border-1 ${active === 'W' ? "bg-[#4A6BFF]/50" : "bg-[#4A6BFF]/10"} rounded-full h-full w-[20%] flex flex-col justify-center items-center md:h-[15%] md:w-[80%] text-[#4A6BFF]`}>
                       <span>W</span>
                     </Link>
 
                     <Link to='/console/workspace/social' 
-                      className='border-1 rounded-full h-full w-[20%] flex flex-col justify-center items-center md:h-[15%] md:w-[80%] text-[#FFD666]'>
+                      onClick={() => setIsActive('S')}
+                      className={`border-1 ${active === 'S' ? "bg-[#FFD666]/50" : "bg-[#FFD666]/10"} rounded-full h-full w-[20%] flex flex-col justify-center items-center md:h-[15%] md:w-[80%] text-[#FFD666]`}>
                       <span>S</span>
                     </Link>
                   {/*  */}
                 </section>
 
-                <section className='w-[20%] md:w-full h-full md:h-[20%] flex flex-row md:flex-col justify-center items-center gap-4 text-[#FFD666]'>
+                <section className='w-full md:w-full h-full md:h-[20%] flex flex-row md:flex-col justify-center items-center gap-4 text-[#FFD666]'>
                   <span className='profile'/>
                   <span className='hidden md:block md:text-xs'>{session?.user.name + "🦥 "}</span>
                 </section>
