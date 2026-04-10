3c8d7631fbb4b87a97dbfdbbf088acc59165d449
Denilson Kuratomi <mhtaue@gmail.com>
2026-04-10 17:29:49 +0800

    app post for items and get for list

--- backend/src/routes/todoList.ts
+++ backend/src/routes/todoList.ts
@@ -2,21 +2,24 @@ import {z} from "zod"
 import { FastifyInstance } from "fastify"
 import { auth } from "shared/auth"
 import { db } from "shared/db";
-import { todoList } from "shared/db/schema";
+import { todoItems, todoList } from "shared/db/schema";
+import { eq } from "drizzle-orm";
+import { session } from "shared/db/schema";
 
 export function todoListApi(app: FastifyInstance){
-    const todoUserSendsShema = z.object({
+    const listSchema = z.object({
         todoName: z.string().min(1),
-        itemName: z.string().min(1),
         description: z.string().min(1)
     })
 
+    const itemSchema = z.object({
+        itemName: z.string().min(1),
+    })
+
     // esse post aqui cria a todoList
-    app.post("/", async(request, reply) =>{
+    app.post("/list", async(request, reply) =>{
         const date = new Date()
-        const doneOrNote = ["done" , "pending"]
         const todoID = crypto.randomUUID()
-        const itemId = crypto.randomUUID()
 
         const session = await auth.api.getSession({
             headers: request.headers
@@ -26,13 +29,13 @@ export function todoListApi(app: FastifyInstance){
             return reply.send({error:"user errado/nao identificado"})
         }
 
-        const response = await todoUserSendsShema.safeParse(request.body)
+        const response = await listSchema.safeParse(request.body)
 
         if(!response.success){
             return reply.status(400).send({error: "erro ao validar a data"})
         }
 
-        const {todoName, itemName, description} = response.data
+        const {todoName, description} = response.data
 
         const [newTodoList] = await db.insert(todoList).values({
             id: todoID,
@@ -46,4 +49,49 @@ export function todoListApi(app: FastifyInstance){
     })
 
     // esse aqui cria um item da todoList
+    app.post('/item/:id', async(request, reply) =>{
+        const { id } = request.params as {id: string}
+        const itemId = crypto.randomUUID()
+        
+        const session = await auth.api.getSession({
+            headers: request.headers
+        }) 
+
+        if(!session) {
+            return reply.send({error:"errro ao validar a session"})
+        }
+
+        const response = itemSchema.safeParse(request.body)
+
+        if(!response.success){
+            return reply.status(400).send({error:"erro a validar os dados"})
+        }
+
+        const {itemName} = response.data
+
+        const [newTodoListItem] = await db.insert(todoItems).values({
+            id: itemId,
+            doneOrNot: "pending",
+            itemName: itemName,
+            todoId: id,
+        }).returning()
+
+        return reply.status(201).send({data: newTodoListItem, message: "item created"})
+    })
+
+    app.get('/by-ids', async(request, reply) => {
+        const session = await auth.api.getSession({
+            headers: request.headers
+        })
+
+        if(!session) {
+            return reply.send({error: "erro na session"})
+        }
+
+        const lists = await db.select()
+        .from(todoList)
+        .where(eq(todoList, todoList.userId))
+
+        return reply.send(lists)
+    })
 }
