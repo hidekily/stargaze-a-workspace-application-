9d5ad9d449c75599c7b24adf6e1332e85c509d4d
Denilson Kuratomi <mhtaue@gmail.com>
2026-04-14 13:41:59 +0800

    app.patch da feature habitos e habitosTracking

--- backend/src/routes/habitos.ts
+++ backend/src/routes/habitos.ts
@@ -1,4 +1,4 @@
-import {z} from "zod"
+import {z, ZodAny} from "zod"
 import { FastifyInstance } from "fastify"
 import { db } from "shared/db"
 import { auth } from "shared/auth"
@@ -106,5 +106,40 @@ export function habitosApi(app: FastifyInstance){
         .where(eq(habitosTracking.id, String(id)))
 
         return reply.send(getTracking)
+    })
+
+    app.patch('/:id', async(request, reply) => {
+        const {id} = request.params as {id: string}
+
+        const schema = z.object({
+            status: z.enum(["done", "pending", "notDone"]).optional(),
+            name: z.string().min(1).optional(),
+            color: z.enum(["#FFD666" , "#FF6B4A" , "#7BA3FF"]).optional()
+        })
+
+        const response = schema.safeParse(request.body)
+
+        if(!response.success){
+            return reply.status(400).send({error: "erro ao validar data"})
+        }
+
+        const {status, name, color} = response.data
+
+        if(name || color){
+            await db
+            .update(habitos)
+            .set({
+                ...(name && {name}),
+                ...(color && {color})
+            })
+            .where(eq(habitos.id, String(id)))
+        }
+
+        if(status){
+            await db
+            .update(habitosTracking)
+            .set({status})
+            .where(eq(habitosTracking.id, String(id)))
+        }
     })
 }
