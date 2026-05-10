38182a0c168a1b01c5432e754eac7c817704db4a
Denilson Kuratomi <mhtaue@gmail.com>
2026-05-10 23:16:38 +0800

    deploy changes

--- shared/src/auth/index.ts
+++ shared/src/auth/index.ts
@@ -7,7 +7,7 @@ export const auth = betterAuth({
     database: drizzleAdapter(db, {
         provider: "pg", 
     }),
-    trustedOrigins: ['http://localhost:3000'],
+    trustedOrigins: ['http://localhost:3000', "https://stargaze-a-workspace-application-fr.vercel.app"],
     baseURL: process.env.BETTER_AUTH_URL || 'http://localhost:3001',
 
     account:{
