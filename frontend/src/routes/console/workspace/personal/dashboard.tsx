ade2840c69c8c9882eaed5f19010ca5b9128e251
Denilson Kuratomi <mhtaue@gmail.com>
2026-05-16 14:53:33 +0800

    inciando o dashboard

--- frontend/src/routes/console/workspace/personal/dashboard.tsx
+++ frontend/src/routes/console/workspace/personal/dashboard.tsx
@@ -1,9 +1,30 @@
+import { API_URL } from '@/lib/api'
 import { createFileRoute } from '@tanstack/react-router'
+import { useQuery } from '@tanstack/react-query'
+import { useMutation } from '@tanstack/react-query'
 
 export const Route = createFileRoute('/console/workspace/personal/dashboard')({
   component: RouteComponent,
 })
 
 function RouteComponent() {
-  return <div>Hello "/console/workspace/personal/dashboard"!</div>
+
+  const {data: todoData} = useQuery({
+    queryKey: [""],
+    queryFn: async() => {
+      const response = await fetch(`${API_URL}/api/todo`, {
+        credentials: 'include',
+        method: 'GET'
+      })
+      return await response.json()
+    }
+  })
+
+  return(
+    <div className='w-full h-full bg-white'>
+      <section className='w-[90%] h-[95%] p-2 bg-black'>
+
+      </section>
+    </div>
+  )
 }
--- shared/src/auth/index.ts
+++ shared/src/auth/index.ts
@@ -1,7 +1,6 @@
 import { betterAuth } from "better-auth"
 import { drizzleAdapter } from "better-auth/adapters/drizzle"
 import { db } from "../db/index.js"
-import { account } from "../db/schema/betterAuth.js"
 
 export const auth = betterAuth({
     database: drizzleAdapter(db, {
