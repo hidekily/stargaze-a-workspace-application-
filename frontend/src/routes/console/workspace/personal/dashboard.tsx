2235b43ac4b7898dc599b65fdfaf43f00af806d7
Denilson Kuratomi <mhtaue@gmail.com>
2026-05-17 13:19:24 +0800

    desenvolvi mais o design do dashboard

--- frontend/src/routes/console/workspace/personal/dashboard.tsx
+++ frontend/src/routes/console/workspace/personal/dashboard.tsx
@@ -2,6 +2,7 @@ import { API_URL } from '@/lib/api'
 import { createFileRoute } from '@tanstack/react-router'
 import { useQuery } from '@tanstack/react-query'
 import { useMutation } from '@tanstack/react-query'
+import { Link } from '@tanstack/react-router'
 
 export const Route = createFileRoute('/console/workspace/personal/dashboard')({
   component: RouteComponent,
@@ -9,21 +10,55 @@ export const Route = createFileRoute('/console/workspace/personal/dashboard')({
 
 function RouteComponent() {
 
-  const {data: todoData} = useQuery({
-    queryKey: [""],
-    queryFn: async() => {
-      const response = await fetch(`${API_URL}/api/todo`, {
-        credentials: 'include',
-        method: 'GET'
-      })
-      return await response.json()
-    }
-  })
+  // const {data: todoData} = useQuery({
+  //   queryKey: [""],
+  //   queryFn: async() => {
+  //     const response = await fetch(`${API_URL}/api/todo`, {
+  //       credentials: 'include',
+  //       method: 'GET'
+  //     })
+  //     return await response.json()
+  //   }
+  // })
 
   return(
-    <div className='w-full h-full bg-white'>
-      <section className='w-[90%] h-[95%] p-2 bg-black'>
+    <div className='w-full h-full flex justify-center items-center'>
+      <section className='w-[93%] h-[95%] p-2 flex flex-col gap-2'>
+        <div className='h-[55%] w-full flex gap-2'>
+          {/* box da financa */}
+          <section className='w-[65%] h-full border-1 border-red-800 rounded-lg'>
+            <section className='h-[20%] flex justify-between'>
+              <p className='p-4 text-red-800 text-2xl'>💸 Finanças</p>
+              <Link to='/console/workspace/personal/financa' className='text-red-800 p-4 text-lg'>ver todas →</Link>
+            </section>
+            <section className='h-[80%]'>
 
+            </section>
+          {/* box das todos */}
+          </section>
+          <section className='w-[35%] h-full border-1 border-red-800 rounded-lg'>
+            <section className='h-[20%] flex justify-between'>
+              <p className='p-4 text-red-800 text-2xl'>📌 todos</p>
+              <Link to='/console/workspace/personal/todo' className='text-red-800 p-4 text-lg'>ver todas →</Link>
+            </section>
+            <section className='h-[80%]'>
+
+            </section>
+          </section>
+        </div>
+
+        <div className='h-[45%] w-full'>
+          <section className='h-full w-full border-1 border-red-800 rounded-lg flex flex-col'>
+            {/* box das notas */}
+            <section className='h-[20%] flex justify-between'>
+              <p className='p-4 text-red-800 text-2xl'>📝 Notas Recentes</p>
+              <Link to='/console/workspace/personal/notas' className='text-red-800 p-4 text-lg'>ver todas →</Link>
+            </section>
+            <section className='h-[80%]'>
+
+            </section>
+          </section>
+        </div>
       </section>
     </div>
   )
